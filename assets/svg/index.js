38dea4645aa2af3b84043d39cd24d727d6e08de3
sbyeol3 <ahnsbyeol3@gmail.com>
2021-04-09 18:34:14 +0900

    Feat: Add Svg Components
    - react-native-svg 라이브러리 사용
    - string으로 xml에 넣어주어야 해서 일단 js 파일에 템플릿 리터럴 사용

---
+++ assets/svg/index.js
@@ -0,0 +1,15 @@
+import add from './add';
+import divider from './divider';
+import line from './line';
+import location from './location';
+import masking from './masking';
+import plus from './plus';
+
+export default {
+  add,
+  divider,
+  line,
+  location,
+  masking,
+  plus,
+};

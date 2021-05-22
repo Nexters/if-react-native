3091cfc5ddcea1cf9b78b2f4e442982b8f3240c5
shiincs <skywing27@gmail.com>
2021-05-22 17:41:40 +0900

    feat: 앨범 피드, 나라 검색 스크린 헤더 추가

--- screens/AlbumFeedScreen.jsx
+++ screens/AlbumFeedScreen.jsx
@@ -3,8 +3,16 @@ import SafeAreaViewLayout from '../components/SafeAreaViewLayout';
 import AlbumFeedList from '../components/AlbumFeedList';
 import { useQuery } from 'react-query';
 import { getCountryFeeds } from '../lib/api/country';
+import Header, { IconItem } from '../components/Header';
+import styled from '@emotion/native';
+import { palette } from '../style/palette';
+import { FontText } from '../components/common';
+import { useNavigation } from '@react-navigation/native';
 
 const AlbumFeedScreen = ({route}) => {
+  const navigation = useNavigation();
+  const navigateToAlbum = () => navigation.navigate('Album');
+
   const countryType = useMemo(() => {
       return route.params.countryType;
   }, [route]);
@@ -21,9 +29,19 @@ const AlbumFeedScreen = ({route}) => {
 
   return (
     <SafeAreaViewLayout includeFlex={true}>
+      <Header>
+        <IconItem iconName="arrow" pressFunction={navigateToAlbum} />
+        <HeaderText>오스트레일리아</HeaderText>
+        <HeaderText>12</HeaderText>
+      </Header>
       <AlbumFeedList />
     </SafeAreaViewLayout>
   );
 };
 
 export default AlbumFeedScreen;
+
+const HeaderText = styled(FontText)`
+  font-size: 18px;
+  color: ${palette.darkbrown};
+`
--- screens/SearchCountryScreen.jsx
+++ screens/SearchCountryScreen.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import SearchCountry from '../components/SearchCountry';
 import SafeAreaViewLayout from '../components/SafeAreaViewLayout';
+import Header, { IconItem } from '../components/Header';
+import styled from '@emotion/native';
+import { FontText } from '../components/common';
+import { palette } from '../style/palette';
+import { useNavigation } from '@react-navigation/native';
 
 const SearchCountryScreen = () => {
+  const navigation = useNavigation();
+  const navigateToPreviousScreen = () => navigation.goBack();
   return (
     <SafeAreaViewLayout includeFlex={true}>
+      <Header>
+        <IconItem iconName="bigCancel" pressFunction={navigateToPreviousScreen} />
+        <HeaderText>여행한 나라 찾기</HeaderText>
+        <HeaderText></HeaderText>
+      </Header>
       <SearchCountry />
     </SafeAreaViewLayout>
   );
 };
 
 export default SearchCountryScreen;
+
+const HeaderText = styled(FontText)`
+  font-size: 18px;
+  color: ${palette.darkbrown};
+`
